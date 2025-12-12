5d302acd3301faf5d196d1c54bc39482cb2059b9
Nicolai-Bak <79926921+Nicolai-Bak@users.noreply.github.com>
2025-12-12 20:37:17 +1000

    setting up pwa

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from 'vite-plugin-pwa'
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Prep Roulette',
+        short_name: 'PrepRoulette',
+        description: 'Randomized meal prep planner',
+      },
+    }),
+  ],
+});
